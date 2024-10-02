8d84b33ae6e3f8605067a3dbdc72b4c14e647143
maykon-jds <maykondias2001@gmail.com>
2024-10-01 22:04:35 -0300

    WIP

--- project/Controller/SignUp.py
+++ project/Controller/SignUp.py
@@ -1,19 +1,49 @@
 from Model.Database import Database
 import streamlit as st
-from types import SimpleNamespace
+from Tools.Validator import Validator
 
 class SignUp:
 
     @staticmethod
-    def register(session_state):
+    def register():
+
+        valid = True
+
+        if "username" in st.session_state:
+            if st.session_state.username == None or st.session_state.username == "":
+                st.error("Username is required!", icon=":material/error:")
+                valid = False
+        else:
+            valid = False
+
+        if "confirmPassword" in st.session_state and "password" in st.session_state:
+
+            if st.session_state.confirmPassword != st.session_state.password:
+                st.error("Passwords don't match!", icon=":material/error:")
+                valid = False
+
+            if (st.session_state.confirmPassword == None and st.session_state.password == None)\
+            or (st.session_state.confirmPassword == "" and st.session_state.password == ""):
+                st.error("Passwords are required!", icon=":material/error:")
+                valid = False
+        else:
+            valid = False
+
+        if "email" in st.session_state:
+            if  st.session_state.email == None or st.session_state.email == "":
+                st.error("E-mail is required!", icon=":material/error:")
+
+            elif not Validator.validateEmail(st.session_state.email):
+                st.error("E-mail is not valid!", icon=":material/error:")
+                valid = False
+        else:
+            valid = False
 
         print(f"Controller: {st.session_state}")
 
-        if (session_state.valid and ("FormSubmitter:signup_form-Submit" in session_state)):
+        if (valid):
 
-            session_state.respose = SimpleNamespace(status="ok", message="User created successfully!")
-
-            st.rerun(scope="fragment")
+            st.session_state.respose = {"status":"ok", "message":"User created successfully!"}
 
 database = Database()
 
--- project/View/SignUp.py
+++ project/View/SignUp.py
@@ -1,16 +1,15 @@
 import streamlit as st
-from Tools.Validator import Validator
 from Controller.SignUp import SignUp
 from types import SimpleNamespace
 
 
-if ("respose" in st.session_state and type(st.session_state.respose) == type(SimpleNamespace())):
+if ("respose" in st.session_state and st.session_state.respose != None):
 
-    if st.session_state.respose.status == "ok":
-        st.success(st.session_state.respose.message)
+    if st.session_state.respose["status"] == "ok":
+        st.success(st.session_state.respose["message"])
 
-    elif st.session_state.respose.status == "error":
-        st.error(st.session_state.respose.message)
+    elif st.session_state.respose["status"] == "error":
+        st.error(st.session_state.respose["message"])
 
     st.session_state.respose = None
 
@@ -19,27 +18,6 @@ st.title("Sign Up")
 
 with st.form("signup_form"):
 
-    st.session_state.valid = True
-
-    if "confirmPassword" in st.session_state and "password" in st.session_state:
-
-        if st.session_state.confirmPassword != st.session_state.password:
-            st.error("Passwords don't match!", icon=":material/error:")
-            st.session_state.valid = False
-
-        if (st.session_state.confirmPassword == None and st.session_state.password == None)\
-        or (st.session_state.confirmPassword == "" and st.session_state.password == ""):
-            st.error("Passwords are required!", icon=":material/error:")
-            st.session_state.valid = False
-    else:
-        st.session_state.valid = False
-
-    if "email" in st.session_state:
-        if  st.session_state.email == None or not Validator.validateEmail(st.session_state.email):
-            st.error("E-mail is not valid!", icon=":material/error:")
-            st.session_state.valid = False
-    else:
-        st.session_state.valid = False
 
     # TODO: Add verification if username already exists in database
 
@@ -53,6 +31,7 @@ with st.form("signup_form"):
 
     confirmPassword = st.text_input(label="Confirm Password", value=None, max_chars=255, key="confirmPassword", type="password")
 
-    st.session_state.signup_formSubmitter = st.form_submit_button("Submit", on_click=SignUp.register, args=[st.session_state])
+    st.session_state.signup_formSubmitter = st.form_submit_button("Submit", on_click=SignUp.register)
+
 
 print(f"View: {st.session_state}")
